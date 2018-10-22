a9a285a0df3ab58f18c3d24d922544a81ed0de7f
Ed Sweeney <ed@onextent.com>
2018-10-21 19:17:20 -0700

    ignore tests

--- src/test/scala/onextent/akka/naviblob/azure/ListBlobsV8Spec.scala
+++ src/test/scala/onextent/akka/naviblob/azure/ListBlobsV8Spec.scala
@@ -10,10 +10,10 @@ class ListBlobsV8Spec extends FlatSpec with Matchers with LazyLogging {
   val storagePath: Option[String] = sys.env.get("BLOB_PATH")
   val containerName: String = sys.env.getOrElse("BLOB_CONTAINER", "unknown")
 
-  implicit val cfg: AzureBlobConfig = AzureBlobConfig(storageAccount, storageKey, containerName, storagePath)
-  implicit val azureBlobber: AzureV8Blobber = new AzureV8Blobber()
-
   ignore should "read blob" in {
+
+    implicit val cfg: AzureBlobConfig = AzureBlobConfig(storageAccount, storageKey, containerName, storagePath)
+    implicit val azureBlobber: AzureV8Blobber = new AzureV8Blobber()
 
     new AzureBlobPaths().toList.headOption match {
       case Some(p) =>
