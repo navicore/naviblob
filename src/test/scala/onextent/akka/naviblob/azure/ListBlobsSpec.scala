d4a259e64306773da2681c6d27b2bdff673a5ac3
Ed Sweeney <ed@onextent.com>
2018-10-25 07:43:07 -0700

    fix test

--- src/test/scala/onextent/akka/naviblob/azure/ListBlobsSpec.scala
+++ src/test/scala/onextent/akka/naviblob/azure/ListBlobsSpec.scala
@@ -1,22 +1,34 @@
 package onextent.akka.naviblob.azure
 
+import com.typesafe.scalalogging.LazyLogging
+import onextent.akka.naviblob.azure.avro.AvroStreamReader
 import onextent.akka.naviblob.azure.storage.{BlobConfig, BlobPaths, Blobber}
 import org.scalatest._
 
-class ListBlobsSpec extends FlatSpec with Matchers {
+class ListBlobsSpec extends FlatSpec with Matchers with LazyLogging {
 
   val storageAccount: String = sys.env.getOrElse("BLOB_ACCOUNT", "unknown")
   val storageKey: String = sys.env.getOrElse("BLOB_KEY", "unknown")
   val storagePath: Option[String] = sys.env.get("BLOB_PATH")
   val containerName: String = sys.env.getOrElse("BLOB_CONTAINER", "unknown")
 
-  ignore should "list blobs" in {
+  ignore should "read blob" in {
 
     implicit val cfg: BlobConfig = BlobConfig(storageAccount, storageKey, containerName, storagePath)
-    implicit val azureBlobber: Blobber = new Blobber
+    implicit val azureBlobber: Blobber = new Blobber()
 
-    val c = new BlobPaths
-    c.foreach(u => println(s"file is $u"))
+    new BlobPaths().toList.headOption match {
+      case Some(p) =>
+
+        val r = new AvroStreamReader[EhRecord](p)
+        val iter = r.read()
+        val records = iter.toList
+
+        records.size should be(12404)
+        records.slice(0, 10).foreach(println)
+
+      case _ => assertResult(false)(true)
+    }
 
   }
 
